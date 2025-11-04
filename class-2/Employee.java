92aed5c8046babfc0bacbe090ac38aa94e9f1000
Narasimha Reddy <49887403+narasimhareddyprostack@users.noreply.github.com>
2025-11-04 12:52:38 +0530

    keywords

---
+++ class-2/Employee.java
@@ -0,0 +1,7 @@
+public class Employee {
+    int 9eid=101;
+    public static void main(String[] args) {
+        
+    }
+
+}
---
+++ class-2/Test.java
@@ -0,0 +1,6 @@
+interface TestI{}
+class Test{
+    public static void main(String[] args) {
+        
+    }
+}
