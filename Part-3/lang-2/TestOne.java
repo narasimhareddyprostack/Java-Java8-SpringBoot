6a02b0c8f40dd08571f9a2927e2f282a6e11e4e0
Narasimha Reddy <49887403+narasimhareddyprostack@users.noreply.github.com>
2025-12-12 12:26:13 +0530

    tS

---
+++ Part-3/lang-2/TestOne.java
@@ -0,0 +1,9 @@
+class Employee extends Object{
+    public static void main(String[] args) {
+        Employee e1=new Employee();
+        Employee e2=new Employee();
+        System.out.println(e1.toString());
+        System.out.println(e2);
+        System.out.println(e1.equals(e2));
+    }
+}
